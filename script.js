6776d29c5ae99b4337916a16f4be3a8ffa45ec96
Ashymaliev <nursultan-99kg@mail.ru>
2021-11-23 13:52:07 +0600

    add slide

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const body = document.body
-
 const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
 
-let activeSlide
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+    if(activeSlide > slides.length - 1) {
+        activeSlide = 0
+    }
+    setActiveSlide()
+    console.log(activeSlide)
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+    if(activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+    setActiveSlide()
+    console.log(activeSlide)
+})
+
+
+function setActiveSlide() {
+    slides.forEach(slide => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
